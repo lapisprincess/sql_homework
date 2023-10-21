b046c171b4d89fe4d720ca953a5144945e5cb7cb
Matilda Lerner <117254835+lapisprincess@users.noreply.github.com>
2023-10-20 20:39:33 -0700

    Update 11.sql

--- 11.sql
+++ 11.sql
@@ -1,3 +1,4 @@
-SELECT DISTINCT productName,  (MSRP * quantityInStock) - (buyPrice * quantityInStock) AS profit
+SELECT DISTINCT productName,  
+  (MSRP * quantityInStock) - (buyPrice * quantityInStock) AS profit
 FROM customers NATURAL JOIN products
 ORDER BY profit
