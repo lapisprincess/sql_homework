153c69eeb5145ac18fefda263474a7a44ea69607
Matilda Lerner <117254835+lapisprincess@users.noreply.github.com>
2023-10-20 20:40:07 -0700

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,4 +1,5 @@
-SELECT customerName, addressLine1, addressLine2, city, state, postalCode
+SELECT customerName, addressLine1, 
+  addressLine2, city, state, postalCode
 FROM customers
 WHERE salesRepEmployeeNumber IS NULL
 ORDER BY customerName
